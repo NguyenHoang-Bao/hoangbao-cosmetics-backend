80f49e2735dbffe3d74db5af4da9b784540d4c5e
NguyenHoang-Bao <hbao4142@gmail.com>
2026-09-22 08:54:53 +0700

    init: Khởi tạo project

---
+++ src/main/java/com/hoangbaoshop/hoangbaocosmetics_backend/HoangbaocosmeticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hoangbaoshop.hoangbaocosmetics_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoangbaocosmeticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoangbaocosmeticsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoangbaoshop/hoangbaocosmetics_backend/HoangbaocosmeticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangbaoshop.hoangbaocosmetics_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoangbaocosmeticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
